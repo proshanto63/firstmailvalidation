eaafefc262ba9903edd078fd82f8b429590f58ce
proshanto63 <42679620+proshanto63@users.noreply.github.com>
2018-10-24 22:55:50 +0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br />
+Your email address is: <?php echo $_POST["email"]; ?><br />
+You Have Ordered This: <?php echo $_POST["code"]; ?><br />
+<?php echo $_POST["comment"]; ?>
+
+</body>
+</html> 
